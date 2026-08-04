e2085ff37f528f2a396a8dda76889c3ad4a65781
04-graphics <004.graphics@gmail.com>
2026-08-04 09:05:28 -0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -36,7 +36,7 @@ function getApiUrl() {
 async function api(action, data = {}) {
   const API_URL = getApiUrl();
 
-  if (!API_URL || API_URL === 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE') {
+  if (!API_URL || API_URL === 'https://script.google.com/macros/s/AKfycbzAnoslatPJvcAo4ooKYI5MlCMwnYjru3ZpnE3PZ3Es7VlJeugFhccFhlEMImD4n3wH/exec') {
     return { success: false, error: 'Apps Script API URL is not configured. Set DEFAULT_API_URL in js/api.js.' };
   }
 
